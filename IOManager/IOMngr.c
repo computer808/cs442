6e5c105ce8b74cf32b29db1855e4dd876b8b9b0e
kohlhage.andrew <kohlhage.andrew@labiMac-016.local>
2019-09-24 15:56:39 -0500

    one small file pointer change

--- IOManager/IOMngr.c
+++ IOManager/IOMngr.c
@@ -3,10 +3,10 @@
  *
  * Date Last Modified: 9/16/2019
  *
- * Driver for IOMngr.h
+ * Driver(?) for IOMngr.h
  */
  #include "IOMngr.h"
- 
+
 int main(int argc, char **argv){
 
 	return 0;
@@ -19,33 +19,33 @@ int main(int argc, char **argv){
  * Return 1 if the file open(s) were successful, otherwise return 0.
  */
 int open_files(const char *source_name, const char *listing_name){
-	FILE *fp_source_name = NULL, fp_listing_name = NULL;
-	
+	FILE *fp_source_name = NULL, *fp_listing_name = NULL;
+
 	//check if source_name is null
 	if(source_name == NULL){
 		fprintf(stderr, "Source_name for file is empty\n");
 		return 0;
 	}
-	
+
 	//open source_name file
 	fp_source_name = fopen(source_name, "r");
 	if(fp_source_name == NULL){
 		fprintf(stderr, "Something went wrong opening the source file\n");
 		return 0;
 	}
-	
+
 	if(listing_name != NULL){
 		fp_listing_name = fopen(listing_name, "w");
 		if(fp_listing_name == NULL){
 			fprintf(stderr, "Something went wrong opening the listing file\n");
 			return 0;
 		}
-		
-		
+
+
 	}
 	else{
-		
+
 	}
-	
+
 	return 1;
 }
